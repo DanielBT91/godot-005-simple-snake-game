d1c432096c714e9632c032e866f1f8d736221a7a
DESKTOP-NV4V396\tablet1 <mrnannings@gmail.com>
2018-06-23 20:36:25 +0200

    prevent food on spawning player or tail;

--- Scenes/World.gd
+++ Scenes/World.gd
@@ -3,6 +3,8 @@ extends Node
 const FOOD = preload("res://Scenes/Food.tscn") 
 
 func _ready():	
+#	randomize()
+
 	var screen_size = OS.get_screen_size()
 	var window_size = OS.get_window_size()	
 	OS.set_window_position(screen_size*0.5 - window_size*0.5)
@@ -15,8 +17,23 @@ func _physics_process(delta):
 		
 func spawn_food():
 	var food = FOOD.instance()
-	var pos = Vector2(round(rand_range(0, 20)), round(rand_range(0, 11))) * 64
-	pos += Vector2(32, 32)
+	
+	var pos
+	for i in 20:
+		
+		pos = Vector2(round(rand_range(0, 20)), round(rand_range(1, 10))) * 64
+		pos += Vector2(32, 32)
+		
+		if pos == $Player.global_position:
+			print("food on player new pos")
+			continue
+		
+		for part in $Player.parts:
+			if pos == part.global_position:
+				print("food on part new pos")
+				continue
+		break			
+		
 	food.global_position = pos
 	add_child(food)
 
